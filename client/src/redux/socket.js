0960d21b31179832946b1b1b0c34b0480d0cffc5
ABDULLAH QIDWAI <abdullahqidwai92@gmail.com>
2026-05-02 11:52:20 +0530

    Update socket connection URL to production server

--- client/src/redux/socket.js
+++ client/src/redux/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-export const socket=io("http://localhost:9000",{
+export const socket=io("https://chat-application-backend-6w7d.onrender.com",{
    autoConnect:true, 
 })
